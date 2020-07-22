6fca21aee7c8e8825ccccbefb11ad1c374d4fbe2
Luzaks <kanemekanik@gmail.com>
2020-07-21 20:37:21 -0500

    Feat: Add infoGame hook.

--- src/components/Games/BuyGame.hook.js
+++ src/components/Games/BuyGame.hook.js
@@ -1,18 +1,23 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { buy_pokemon_action, return_pokemon_action } from '../../actions/actions';
 
 const BuyGameHook = () => {
+
+    const dispatch = useDispatch();
+
     return (
         <div>
             <button className="btn btn-dark btn-sm mb-2"
                     onClick={() => {
-
+                        dispatch(buy_pokemon_action(1));
                     }
                     }>
                 Buy
             </button>
             <button className="btn btn-dark btn-sm mb-1"
                     onClick={() => {
-
+                        dispatch(return_pokemon_action(1));
                     }}>
                 Return
             </button>
