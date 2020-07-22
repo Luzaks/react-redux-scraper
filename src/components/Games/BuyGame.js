76a7c4857fab3c54e7434cb8afccbc3e370563b0
Luzaks <kanemekanik@gmail.com>
2020-07-21 20:22:06 -0500

    Feat: Add mapdispatchToProps function and complete the Redux cycle.

--- src/components/Games/BuyGame.js
+++ src/components/Games/BuyGame.js
@@ -1,13 +1,22 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { buy_pokemon_action, return_pokemon_action } from '../../actions/actions'
 
 class BuyGame extends Component {
     render() {
         return (
             <div>
-                <button className="btn btn-dark btn-sm mb-2">
+                <button className="btn btn-dark btn-sm mb-2"
+                        onClick={() => {
+                             this.props.buy_pokemon_action(1);
+                            }
+                        }>
                     Buy
                 </button>
-                <button className="btn btn-dark btn-sm mb-1">
+                <button className="btn btn-dark btn-sm mb-1"
+                        onClick={() => {
+                            this.props.return_pokemon_action(1);
+                        }}>
                     Return
                 </button>
             </div>
@@ -15,4 +24,9 @@ class BuyGame extends Component {
     }
 }
 
-export default BuyGame;
+const mapDispatchToProps = {
+    buy_pokemon_action,
+    return_pokemon_action
+};
+
+export default connect(null, mapDispatchToProps)(BuyGame);
--- src/store/store.js
+++ src/store/store.js
@@ -1,20 +1,12 @@
 import {createStore} from "redux";
 import rootReducer from "../reducers/rootReducer";
 import initialState from "../state/initialState";
-import {buy_pokemon_action} from "../actions/actions";
 import { composeWithDevTools } from 'redux-devtools-extension'
-
 
 const store = createStore(
     rootReducer,
     initialState,
     composeWithDevTools()
 );
-console.log('inicial:', store.getState());
-store.subscribe(() => {
-    console.log('cambio:', store.getState());
-});
-
-store.dispatch(buy_pokemon_action(1));
 
 export default store;
