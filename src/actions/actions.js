11a34365eb7fc8ee5be89d8b651a2e1d902d1b51
Luzaks <kanemekanik@gmail.com>
2020-07-21 21:41:45 -0500

    Feat: Add asyncronous actions.

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,7 +1,12 @@
-import { BUY_POKEMON,
-         RETURN_POKEMON,
-         BUY_CONSOLE,
-         RETURN_CONSOLE } from './actionTypes';
+import {
+    BUY_POKEMON,
+    RETURN_POKEMON,
+    BUY_CONSOLE,
+    RETURN_CONSOLE,
+    FETCH_REQUEST,
+    FETCH_SUCCESS, FETCH_FAILURE
+} from './actionTypes';
+import Axios from 'axios';
 
 const buy_pokemon_action = (cant) => {
     return {
@@ -31,9 +36,47 @@ const return_console_action = (cant) => {
     }
 };
 
+const fetch_request = () => {
+    return {
+        type: FETCH_REQUEST
+    }
+};
+
+const fetch_success = (pokemon) => {
+    return {
+        type: FETCH_SUCCESS,
+        payload: pokemon
+    }
+};
+
+const fetch_failure = (error) => {
+    return {
+        type: FETCH_FAILURE,
+        payload: error
+    }
+};
+
+const fetch_pokemon = (value) => {
+    return (dispatch) => {
+        dispatch(fetch_request());
+        Axios.get(`https://pokeapi.co/api/v2/pokemon/${value}`)
+            .then((r) => {
+                dispatch(fetch_success([r.data]))
+             })
+            .catch(onerror => {
+                dispatch(fetch_failure('Ups.'))
+            });
+    }
+};
+
 export {
     buy_pokemon_action,
     return_pokemon_action,
     buy_console_action,
-    return_console_action
+    return_console_action,
+    fetch_request,
+    fetch_success,
+    fetch_failure,
 }
+
+export default fetch_pokemon;
