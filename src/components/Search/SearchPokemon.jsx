640c9891f4c57e4d42f39c417512dd63ec5b6b05
Luzaks <kanemekanik@gmail.com>
2020-07-21 22:05:53 -0500

    Feat: Add hooks for dynamic state in search input.

--- src/components/Search/SearchPokemon.jsx
+++ src/components/Search/SearchPokemon.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, {useState} from 'react';
+import {useDispatch} from 'react-redux';
+import fetch_pokemon from '../../actions/actions';
 
 const SearchPokemon = () => {
+    const dispatch = useDispatch();
+    const [name, set_name] = useState('mewtwo');
     return (
         <div className="form-group">
             <label htmlFor="search" className="text-white">Search Pokemon</label>
-            <input type="text" className="form-control" id="search" value="pikachu" />
-            <button className="btn btn-primary mt-3">Submit</button>
+            <input type="text" className="form-control" id="search"
+                   value={name}
+                    onChange={
+                        (ev) => {
+                            set_name(ev.target.value);
+                        }
+                    }/>
+            <button className="btn btn-primary mt-3"
+                    onClick={() => {
+                        dispatch(fetch_pokemon(name));
+                    }}>
+                Submit
+            </button>
         </div>
     );
 };
--- src/store/store.js
+++ src/store/store.js
@@ -1,12 +1,15 @@
-import {createStore} from "redux";
-import rootReducer from "../reducers/rootReducer";
-import initialState from "../state/initialState";
+import {applyMiddleware, createStore} from 'redux';
+import rootReducer from '../reducers/rootReducer';
+import initialState from '../state/initialState';
 import { composeWithDevTools } from 'redux-devtools-extension'
+import thunk from 'redux-thunk';
 
 const store = createStore(
     rootReducer,
     initialState,
-    composeWithDevTools()
+    composeWithDevTools(
+        applyMiddleware(thunk)
+    )
 );
 
 export default store;
